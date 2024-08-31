4a19efe79c62c9f0a92159caa7238ffe1cb4ca39
tacascer <trandangtrithanh2000@gmail.com>
2024-08-31 19:32:18 -0400

    build: read from kover report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.kotlinx.kover)
     alias(libs.plugins.sonarqube)
 }
 
@@ -6,5 +7,13 @@ sonar {
     properties {
         property("sonar.projectKey", "tacascer-org_gradle-monorepo")
         property("sonar.organization", "tacascer-org")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.asFile.get()}/reports/kover/report.xml",
+        )
     }
+}
+
+dependencies {
+    kover(project(":predix"))
 }

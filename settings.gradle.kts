de6867f6550549569e20b1d8eea8d15065103942
tacascer <trandangtrithanh2000@gmail.com>
2024-08-31 16:39:24 -0400

    build: initial Gradle setup

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.gradle.develocity") version "3.17.4"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+    }
+}
+
+rootProject.name = "gradle-monorepo"

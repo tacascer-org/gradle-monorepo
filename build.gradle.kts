faa00b1a995559f2fad024cb9ed0e085026e52b3
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-09-01 18:57:59 -0400

    ci: format version catalog (#15)
    * ci: format version catalog
    
    * ci: set up Gradle before
    
    * chore: fix linting issues
    
    * ci: set up lint lifecycle task
    
    * chore: fix linting issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlinx.kover)
+    alias(libs.plugins.benManes.versions)
     alias(libs.plugins.diffplug.spotless)
+    alias(libs.plugins.littlerobots.versionCatalogUpdate)
 }
 
 group = "io.github.tacascer"
@@ -10,4 +11,19 @@ spotless {
         target("*.kts")
         ktlint()
     }
+}
+val globalBuildGroup = "monorepo build"
+
+val lint by tasks.registering {
+    group = globalBuildGroup
+    dependsOn(tasks.spotlessApply)
+    dependsOn(tasks.versionCatalogFormat)
+}
+
+tasks.named<TaskReportTask>("tasks") {
+    displayGroups = listOf(globalBuildGroup)
+}
+
+tasks.check {
+    dependsOn(lint)
 }

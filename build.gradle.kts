bedd917a37243b1b350e21d5850f4e865099f39d
tacascer <trandangtrithanh2000@gmail.com>
2024-09-01 18:10:49 -0400

    ci: format version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinx.kover)
     alias(libs.plugins.diffplug.spotless)
+    alias(libs.plugins.littlerobots.versionCatalogUpdate)
 }
 
 group = "io.github.tacascer"

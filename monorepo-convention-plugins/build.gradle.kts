6e2302f9ff065f4f9573a671ea3c29a4b230ed62
tacascer <trandangtrithanh2000@gmail.com>
2024-09-02 00:15:11 -0400

    test: add tests to settings plugin

--- monorepo-convention-plugins/build.gradle.kts
+++ monorepo-convention-plugins/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.sonarqube)
+    alias(libs.plugins.kotlinx.kover)
 }
 
 version = "0.0.1" // x-release-please-version
@@ -13,11 +14,23 @@ val qualityCheckAll by tasks.registering { }
 
 val buildAll by tasks.registering { }
 
+dependencies {
+    kover(project(":settings-convention-plugin"))
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "tacascer-org_gradle-monorepo_convention-plugins")
         property("sonar.organization", "tacascer-org")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.asFile.get()}/reports/kover/report.xml",
+        )
     }
+}
+
+tasks.sonar {
+    dependsOn(tasks.koverXmlReport)
 }
 
 qualityCheckAll {

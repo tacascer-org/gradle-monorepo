8914b341a5a86bb0fa6d4e6078df14cdd03d64e1
tacascer <trandangtrithanh2000@gmail.com>
2024-09-08 00:58:24 -0400

    build: add sonar

--- pto-scheduler/build.gradle.kts
+++ pto-scheduler/build.gradle.kts
@@ -1,6 +1,30 @@
 plugins {
     id("kotlin-spring-conventions")
+    alias(libs.plugins.sonarqube)
 }
 
 group = "com.github.lowkeylab"
 version = "0.0.1" // x-release-please-version
+
+tasks.sonar {
+    dependsOn(tasks.koverXmlReport)
+}
+
+tasks.qualityCheck {
+    dependsOn(tasks.sonar)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "lowkeylab_gradle-monorepo_pto-scheduler")
+        property("sonar.organization", "lowkeylab")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.asFile.get()}/reports/kover/report.xml",
+        )
+        property(
+            "sonar.userHome",
+            "${layout.buildDirectory.asFile.get()}/.sonar",
+        )
+    }
+}

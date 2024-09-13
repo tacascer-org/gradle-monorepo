5ad34d96e362a8f9abf329dbe87d09106a3ea0a3
tacascer <trandangtrithanh2000@gmail.com>
2024-09-13 00:21:47 -0400

    refactor: remove save from UserRepository

--- pto-scheduler/src/main/kotlin/com/github/lowkeylab/ptoscheduler/user/db/UserRepository.kt
+++ pto-scheduler/src/main/kotlin/com/github/lowkeylab/ptoscheduler/user/db/UserRepository.kt
@@ -3,6 +3,4 @@ package com.github.lowkeylab.ptoscheduler.user.db
 import com.github.lowkeylab.ptoscheduler.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long> {
-    fun save(user: User): User
-}
+interface UserRepository : JpaRepository<User, Long>
